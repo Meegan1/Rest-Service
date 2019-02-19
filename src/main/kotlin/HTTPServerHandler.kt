077f8c728ee62b7c5875ff69bcc8f03624827072
Jake Meegan <meegan118@gmail.com>
2019-02-19 01:00:31 +0000

    gitflow-feature-stash: initial-server

---
+++ src/main/kotlin/HTTPServerHandler.kt
@@ -0,0 +1,53 @@
+package me.meegan.rest
+
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory
+import org.glassfish.jersey.process.Inflector
+import org.glassfish.jersey.server.ResourceConfig
+import org.glassfish.jersey.server.model.Resource
+import java.net.URI
+import javax.ws.rs.container.ContainerRequestContext
+import javax.ws.rs.core.Response
+
+
+class HTTPServerHandler {
+    private val BASE_URI = URI.create("http://localhost:8080/api/")
+    private var resourceConfig = create()
+    private var server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, resourceConfig, false)
+
+    init {
+            this.start()
+            Runtime.getRuntime().addShutdownHook(Thread(Runnable { this.server.shutdownNow() }))
+    }
+
+    fun start() {
+        if (this.server.isStarted) stop()
+        server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, resourceConfig, false)
+        this.server.start()
+    }
+
+    fun stop() {
+        this.server.shutdownNow()
+        resourceConfig = ResourceConfig(resourceConfig)
+    }
+
+    fun create() : ResourceConfig {
+        val resourceBuilder = Resource.builder("hello")
+        resourceBuilder.addMethod("GET").handledBy {
+            Response.ok("Hello World!").build()
+        }
+
+        val noContentResponder = Inflector<ContainerRequestContext, Response> {
+            Response.noContent().build()
+        }
+        resourceBuilder.addMethod("HEAD").handledBy(noContentResponder)
+        resourceBuilder.addMethod("OPTIONS").handledBy(noContentResponder)
+
+        return ResourceConfig().registerResources(resourceBuilder.build())
+    }
+
+    fun registerCommand(resource : Resource) {
+        if(server.isStarted) stop()
+        resourceConfig.registerResources(resource)
+        start()
+    }
+}
---
+++ src/test/kotlin/HTTPServerHandlerTest.kt
@@ -0,0 +1,26 @@
+import me.meegan.rest.HTTPServerHandler
+import org.glassfish.jersey.server.model.Resource
+import javax.ws.rs.core.Response
+
+internal class HTTPServerHandlerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    fun tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun start() {
+        val server = HTTPServerHandler()
+        val resourceBuilder = Resource.builder("hi")
+        resourceBuilder.addMethod("GET").handledBy {
+            Response.ok("Hi World!").build()
+        }
+
+        server.registerCommand(resourceBuilder.build())
+        readLine()
+    }
+}
